b2ae9b2ffd70eb15cbeff8f4788544d57b846caa
Morifeoluwa <morife@cottacush.com>
2016-04-18 10:56:22 +0100

    fixed all issues with ChocolateFeast!

--- src/Algorithm/ChocolateFeast.java
+++ src/Algorithm/ChocolateFeast.java
@@ -6,13 +6,23 @@ import java.util.Scanner;
  * Created by Morifeoluwa on 4/15/2016.
  */
 public class ChocolateFeast {
+    public static int quantity(int capital, int price, int discount){
+
+        int wraps = (capital / price);
+        int discountWraps = wraps / discount;
+        int sheddingWraps = ((wraps % discount) + discountWraps) / discount;
+        int totalWraps = wraps + discountWraps + sheddingWraps;
+        return totalWraps;
+    }
     public static void main (String[]args){
         Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for (int i = 0; i < T; i++){
         int capital = in.nextInt();
         int price = in.nextInt();
         int discount = in.nextInt();
 
-        int quantity = (capital /price) + ((capital / price) / discount);
-        System.out.println(quantity);
+        System.out.println(quantity(capital,price,discount));
+        }
     }
 }
